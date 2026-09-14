d0b01c163ee40f654cbd4e711320b52596d1e1d7
Camilo Ortiz <cristian.ortiz-s@mail.escuelaing.edu.co>
2026-09-13 20:16:38 -0500

    feat: GREEN - soporta tiro con 0 pinos

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package edu.eci.dosw.bowling;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
@@ -8,11 +9,11 @@ import org.junit.jupiter.api.Test;
 public class BowlingGameTest {
 
     @Test
-    @DisplayName("roll(-1) lanza IllegalArgumentException")
-    void rollNegativePins_throwsException() {
+    @DisplayName("roll(0) No lanza excepción. El frame registra 0 pinos.")
+    void shouldRollZeroPins() {
         // Arrange
         BowlingGame game = new BowlingGame();
         // Act & Assert
-        assertThrows(IllegalArgumentException.class, () -> game.roll(-1));
+        assertDoesNotThrow(() -> game.roll(0));
     }
 }

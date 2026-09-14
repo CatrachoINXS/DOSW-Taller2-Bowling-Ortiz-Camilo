d2d82fa93b8692d3c34dec53608bf6cf2c66a11c
Camilo Ortiz <cristian.ortiz-s@mail.escuelaing.edu.co>
2026-09-13 20:21:12 -0500

    test: RED - roll lanza excepcion negativo

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -16,4 +16,14 @@ public class BowlingGameTest {
         // Act & Assert
         assertDoesNotThrow(() -> game.roll(0));
     }
+
+    @Test
+    @DisplayName("roll(-1) lanza IllegalArgumentException")
+    void rollNegativePins_throwsException() {
+        // Arrange
+        BowlingGame game = new BowlingGame();
+        // Act & Assert
+        assertThrows(IllegalArgumentException.class, () -> game.roll(-1));
+    }
+
 }

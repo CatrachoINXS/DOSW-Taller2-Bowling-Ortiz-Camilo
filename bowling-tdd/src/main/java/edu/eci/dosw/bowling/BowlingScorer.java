69c267c22130af65676cc54ac6f0a18012588856
Camilo Ortiz <cristian.ortiz-s@mail.escuelaing.edu.co>
2026-09-14 10:42:51 -0500

    refactor: extrae suma del puntaje de los frames

--- bowling-tdd/src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
+++ bowling-tdd/src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class BowlingScorer {
 
     public static int calculate(List<Frame> frames) {
-        int score = frames.stream().mapToInt(Frame::getPins).sum();
-        return score;
+        return frames.stream().mapToInt(Frame::getPins).sum();
     }
 }

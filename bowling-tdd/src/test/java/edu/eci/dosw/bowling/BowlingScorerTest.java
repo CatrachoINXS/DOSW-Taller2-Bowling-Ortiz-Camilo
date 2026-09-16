25cf3e5d361aab805559ee4c80f0f1f3949827c1
Camilo Ortiz <cristian.ortiz-s@mail.escuelaing.edu.co>
2026-09-16 14:53:16 -0500

    refactor: Issues relevantes de SonarQube corregidos

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
@@ -1,5 +1,32 @@
 package edu.eci.dosw.bowling;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BowlingScorerTest {
     
+    @Test
+    @DisplayName("El puntaje de un juego normal se calcula correctamente")
+    void shouldCalculateTheScoreOfANormalGameCorrectly() {
+        BowlingGame game = new BowlingGame();
+        rollMany(game, 20, 3);
+
+        assertEquals(60, game.score());
+    }
+
+    @Test
+    @DisplayName("El puntaje de un juego con strikes se calcula correctamente")
+    void shouldCalculateTheScoreOfAGameWithStrikesCorrectly() {
+        BowlingGame game = new BowlingGame();
+        rollMany(game, 5, 10);
+        rollMany(game, 10, 4);
+
+        assertEquals(172, game.score());
+    }
+
+    private void rollMany(BowlingGame game, int times, int pins) {
+        for (int i = 0; i < times; i++) game.roll(pins);
+    }
 }
